cc0f17a5a5d606f30acfe78849573d6dd8be911d
rafael <rafaelmagalhaes1@live.com>
2024-03-15 19:39:27 -0300

    feat: Status Code Map created

---
+++ src/utils/StatusCode.ts
@@ -0,0 +1,19 @@
+//
+const statusCode = (status: string): number => {
+  //
+  switch (status) {
+    //
+    case 'SUCCESSFUL': return 200;
+    case 'CREATED': return 201;
+    case 'INVALID_DATA': return 400;
+    case 'UNAUTHORIZED': return 401;
+    case 'NOT_FOUND': return 404;
+    case 'CONFLICT': return 409;
+    case 'UNPROCESSABLE_ENTITY': return 422;
+    case 'INTERNAL_ERROR': return 500;
+
+    default: return 500;
+  }
+}
+
+export default statusCode;
