83ed9d7968e56d73f2f402087423fb963f6cba75
rafael <rafaelmagalhaes1@live.com>
2024-03-15 17:51:42 -0300

    feat: class Server created

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import App from './app';
+
+class Server {
+  private app: App;
+  private port: string | number;
+
+  constructor(port: string | number) {
+    this.app = new App();
+    this.port = port;
+  }
+
+  public start() {
+    this.app.start(this.port);
+  }
+}
+
+const PORT = process.env.APP_PORT || 3001;
+new Server(PORT).start();
