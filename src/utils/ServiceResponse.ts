0917b995eb4ceeb2703d78bf9041370787e3799c
rafael <rafaelmagalhaes1@live.com>
2024-03-15 19:39:41 -0300

    feat: ServiceResponse created

---
+++ src/utils/ServiceResponse.ts
@@ -0,0 +1,17 @@
+//
+export type ServiceMessage = { message: string };
+
+type ServiceResponseErrorType = 'INVALID_DATA' | 'UNAUTHORIZED' | 'NOT_FOUND' | 'CONFLICT' |
+'UNPROCESSABLE_ENTITY' | 'INTERNAL_ERROR';
+
+export type ServiceResponseError = {
+  status: ServiceResponseErrorType,
+  data: ServiceMessage
+};
+
+export type ServiceResponseSuccess<T> = {
+  status: 'SUCCESSFUL' | 'CREATED',
+  data: T
+};
+
+export type ServiceResponse<T> = ServiceResponseError | ServiceResponseSuccess<T>;
