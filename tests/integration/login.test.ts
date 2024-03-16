5228fe8aa522b4797104cb4ede3999ed49ca869f
rafael <rafaelmagalhaes1@live.com>
2024-03-16 07:34:06 -0300

    fix: tests integration

---
+++ tests/integration/login.test.ts
@@ -0,0 +1,86 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import app from '../../src/app';
+import JwtService from '../../src/utils/JwtService';
+import { tokenMock } from '../mocks/user.mock';
+import LoginService from '../../src/services/LoginService';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Login tests', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  describe('1. Integration tests', () => {
+    it('should return token and status 200', async function() {
+      // arrange
+      sinon.stub(JwtService, 'createToken').returns(tokenMock);
+        
+      // act
+      const request = { email: 'user@user.com', password: 'secret_user' };
+    
+      const res = await chai.request(app).post('/login').send(request);
+  
+      // assert
+      expect(res.status).to.equal(200);
+      expect(res.body).to.deep.equal({ token: tokenMock })
+    });
+
+    it('should return role and status 200', async function() {
+      // arrange
+      const email = 'test@example.com';
+      const role = 'admin';
+      sinon.stub(JwtService, 'verifyToken').returns({ email, role } as any);
+      sinon.stub(LoginService.prototype, 'getRole').withArgs(email).resolves({ status: 'SUCCESSFUL', data: { role } });
+    
+      // act
+      const res = await chai.request(app).get('/login/role').set('Authorization', `Bearer ${tokenMock}`);
+    
+      // assert
+      expect(res.status).to.equal(200);
+      expect(res.body).to.deep.equal({ role });
+    });
+
+    it('should return 400 if email is not provided', async function() {
+      // act
+      const { status, body } = await chai.request(app).post('/login').send({ password: '123456' });
+
+      // assert
+      expect(status).to.equal(400);
+      expect(body).to.deep.equal({ message: 'All fields must be filled' });
+    });
+
+    it('should return 400 if password is not provided', async function() {
+      // act
+      const { status, body } = await chai.request(app).post('/login').send({ email: 'rafael@gmail.com' });
+
+      // assert
+      expect(status).to.equal(400);
+      expect(body).to.deep.equal({ message: 'All fields must be filled' });
+    });
+
+    it('should return 401 if email is invalid', async function() {
+      // act
+      const { status, body } = await chai.request(app).post('/login').send({ email: 'rafael@gmail.com', password: '123456' });
+
+      // assert
+      expect(status).to.equal(401);
+      expect(body).to.deep.equal({ message: 'Invalid email or password' });
+    });
+
+    it('should return 401 if password is invalid', async function() {
+      // act
+      const { status, body } = await chai.request(app).post('/login').send({ email: 'rafael@gmail.com', password: '123456' });
+
+      // assert
+      expect(status).to.equal(401);
+      expect(body).to.deep.equal({ message: 'Invalid email or password' });
+    });
+  });
+});
---
+++ tests/mocks/user.mock.ts
@@ -0,0 +1,10 @@
+export const userMock = {
+    id: 1,
+    username: 'User',
+    role: 'user',
+    email: 'user@user.com',
+    password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO'
+  }
+  
+  export const tokenMock = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXJAdXNlci5jb20iLCJyb2xlIjoidXNlciIsImlhdCI6MTcwOTU1NDE2NCwiZXhwIjoxNzA5NTU3NzY0fQ.MHdAnIP54zcm-TwlUoxF1xaLEgtlp6fuN5mbDLfPaDU';
+  
