89908099a7b63e7f7f74df796f0cd50a7eb5c4c1
rafael <rafaelmagalhaes1@live.com>
2024-03-28 20:38:27 -0300

    tests: unit tests to updateUser on services created

--- tests/unit/userService.test.ts
+++ tests/unit/userService.test.ts
@@ -10,69 +10,115 @@ describe('Unit Tests UserService', () => {
    sinon.restore();
   });
 
-  it('1.1 - should get all users', async () => {
-    // Arrange
-    sinon.stub(UserModel.prototype, 'getAllUsers').returns(usersMock as any);
+  describe('1.1 - getAllUsers', () => {
+    it('1.1 - should get all users', async () => {
+      // Arrange
+      sinon.stub(UserModel.prototype, 'getAllUsers').returns(usersMock as any);
 
-    // Act
-    const userService = new UserService(new UserModel());
-    const users = await userService.getAllUsers();
+      // Act
+      const userService = new UserService(new UserModel());
+      const users = await userService.getAllUsers();
 
-    // Assert
-    expect(users.data).to.eql(usersMock);
+      // Assert
+      expect(users.data).to.eql(usersMock);
+    });
+
+    it('1.2 - should failed to get all users', async () => {
+      // Arrange
+      sinon.stub(UserModel.prototype, 'getAllUsers').returns(null as any);
+
+      // Act
+      const userService = new UserService(new UserModel());
+      const users = await userService.getAllUsers();
+
+      // Assert
+      expect(users.status).to.eql('INTERNAL_ERROR');
+      expect(users.data).to.eql({ message: 'Failed to get users' });
+    });
   });
 
-  it('1.2 - should failed to get all users', async () => {
-    // Arrange
-    sinon.stub(UserModel.prototype, 'getAllUsers').returns(null as any);
+  describe('1.2 - registerUser', () => {
+    it('1.3 - should register a user', async () => {
+      // Arrange
+      sinon.stub(UserModel.prototype, 'getByEmail').returns(Promise.resolve(null));
+      sinon.stub(UserModel.prototype, 'createUser').returns(userMockUpdate as any);
+      sinon.stub(bcrypt, 'hashSync').returns('hashedpassword');
 
-    // Act
-    const userService = new UserService(new UserModel());
-    const users = await userService.getAllUsers();
+      // Act
+      const userService = new UserService(new UserModel());
+      const user = await userService.registerUser(userMockCreate as any);
 
-    // Assert
-    expect(users.status).to.eql('INTERNAL_ERROR');
-    expect(users.data).to.eql({ message: 'Failed to get users' });
+      // Assert
+      expect(user.data).to.eql(userMockUpdate);
+    });
+
+    it('1.4 - should not register a user with an existing email', async () => {
+      // Arrange
+      sinon.stub(UserModel.prototype, 'getByEmail').returns(userMock as any);
+
+      // Act
+      const userService = new UserService(new UserModel());
+      const user = await userService.registerUser(userMockCreate as any);
+
+      // Assert
+      expect(user.status).to.eql('CONFLICT');
+      expect(user.data).to.eql({ message: 'User already exists' });
+    });
+
+    it('1.5 - should failed to register a user', async () => {
+      // Arrange
+      sinon.stub(UserModel.prototype, 'getByEmail').returns(Promise.resolve(null));
+      sinon.stub(UserModel.prototype, 'createUser').returns(null as any);
+
+      // Act
+      const userService = new UserService(new UserModel());
+      const user = await userService.registerUser(userMockCreate as any);
+
+      // Assert
+      expect(user.status).to.eql('INTERNAL_ERROR');
+      expect(user.data).to.eql({ message: 'Failed to register user' });
+    });
   });
 
-  it('1.3 - should register a user', async () => {
-    // Arrange
-    sinon.stub(UserModel.prototype, 'getByEmail').returns(Promise.resolve(null));
-    sinon.stub(UserModel.prototype, 'createUser').returns(userMockUpdate as any);
-    sinon.stub(bcrypt, 'hashSync').returns('hashedpassword');
+  describe('1.6 - updateUser', () => {
+    it('1.6.1 - should return user updated', async function() {
+      // arrange
+      sinon.stub(UserModel.prototype, 'getById').resolves(userMockUpdate as any);
+      sinon.stub(UserModel.prototype, 'updateUser').resolves(userMockUpdate as any);
 
-    // Act
-    const userService = new UserService(new UserModel());
-    const user = await userService.registerUser(userMockCreate as any);
+      // act
+      const userService = new UserService(new UserModel());
+      const user = await userService.updateUser(1, userMockUpdate as any);
 
-    // Assert
-    expect(user.data).to.eql(userMockUpdate);
-  });
+      // assert
+      expect(user.data).to.eql(userMockUpdate);
+    });
 
-  it('1.4 - should not register a user with an existing email', async () => {
-    // Arrange
-    sinon.stub(UserModel.prototype, 'getByEmail').returns(userMock as any);
+    it('1.6.2 - should return user not found', async function() {
+      // arrange
+      sinon.stub(UserModel.prototype, 'getById').resolves(null as any);
 
-    // Act
-    const userService = new UserService(new UserModel());
-    const user = await userService.registerUser(userMockCreate as any);
+      // act
+      const userService = new UserService(new UserModel());
+      const user = await userService.updateUser(1, userMockUpdate as any);
 
-    // Assert
-    expect(user.status).to.eql('CONFLICT');
-    expect(user.data).to.eql({ message: 'User already exists' });
-  });
+      // assert
+      expect(user.status).to.eql('INTERNAL_ERROR');
+      expect(user.data).to.eql({ message: 'User already exists' });
+    });
 
-  it('1.5 - should failed to register a user', async () => {
-    // Arrange
-    sinon.stub(UserModel.prototype, 'getByEmail').returns(Promise.resolve(null));
-    sinon.stub(UserModel.prototype, 'createUser').returns(null as any);
+    it('1.6.3 - should return failed to update user', async function() {
+      // arrange
+      sinon.stub(UserModel.prototype, 'getById').resolves(userMockUpdate as any);
+      sinon.stub(UserModel.prototype, 'updateUser').resolves(null as any);
 
-    // Act
-    const userService = new UserService(new UserModel());
-    const user = await userService.registerUser(userMockCreate as any);
+      // act
+      const userService = new UserService(new UserModel());
+      const user = await userService.updateUser(1, userMockUpdate as any);
 
-    // Assert
-    expect(user.status).to.eql('INTERNAL_ERROR');
-    expect(user.data).to.eql({ message: 'Failed to register user' });
+      // assert
+      expect(user.status).to.eql('INTERNAL_ERROR');
+      expect(user.data).to.eql({ message: 'Failed to update user' });
+    });
   });
 });
