0a76967e7d367005bc630d542516bc4293163b87
rafael <rafaelmagalhaes1@live.com>
2024-03-28 14:57:31 -0300

    feat: added unit tests to JwtService

--- tests/unit/authMiddleware.test.ts
+++ tests/unit/authMiddleware.test.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express';
 import { Authenticate } from '../../src/middlewares/AuthMiddleware';
 import { JwtService } from '../../src/utils/JwtService';
 
-describe('Authenticate', () => {
+describe('Unit Tests Authenticate', () => {
   let req: Partial<Request>;
   let res: Partial<Response>;
   let next: sinon.SinonStub;
@@ -26,7 +26,7 @@ describe('Authenticate', () => {
     sinon.restore();
   });
 
-  it('should return 401 if no authorization header', async () => {
+  it('1.1 - should return 401 if no authorization header', async () => {
     // Arrange
     delete req.headers?.authorization;
     
@@ -38,11 +38,11 @@ describe('Authenticate', () => {
     sinon.assert.calledWith(res.json as sinon.SinonStub, { message: 'Token not found' });
   });
 
-  it('should return 401 if token is invalid', async () => {
+  it('1.2 - should return 401 if token is invalid', async () => {
     sinon.stub(JwtService, 'splitToken').throws();
   });
 
-  it('should error validate token', async () => {
+  it('1.3 - should error validate token', async () => {
     // Arrange
     sinon.stub(JwtService, 'verifyToken').throws();
     
