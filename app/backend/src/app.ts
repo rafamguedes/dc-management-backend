7b57182310f5fc52799239c66e5e21c2ec682c3c
rafael <rafaelmagalhaes1@live.com>
2024-03-29 17:15:27 -0300

    feat: config cors on backend

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -3,16 +3,26 @@ import express = require('express');
 import MainRoutes from './routes';
 
 class App {
-  //
   public app: express.Express;
-  
   private mainRoutes: MainRoutes = new MainRoutes();
 
   constructor() {
     this.app = express();
     this.mainRoutes = new MainRoutes();
-    this.app.use(express.json());
+    this.config();
     this.routes();
+  }
+
+  private config():void {
+    const accessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+      res.header('Access-Control-Allow-Headers', '*');
+      next();
+    };
+
+    this.app.use(express.json());
+    this.app.use(accessControl);
   }
 
   private routes(): void {
