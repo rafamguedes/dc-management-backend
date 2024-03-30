cfd0f2ef81216a42b869d9ef6a9958f98c00d44d
rafael <rafaelmagalhaes1@live.com>
2024-03-30 16:46:13 -0300

    feat: hook useDashboard created

---
+++ app/frontend/src/hooks/useDashboard.ts
@@ -0,0 +1,119 @@
+import Swal from 'sweetalert2';
+import { useNavigate } from 'react-router';
+import { User } from '../types/UserTypes';
+import { useState, useEffect } from 'react';
+import ApiService from '../services/UserService';
+
+const useDashboard = () => {
+  const navigate = useNavigate();
+  const [users, setUsers] = useState<User[]>([]);
+  const [allUsers, setAllUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [editingId, setEditingId] = useState<number | null>(null);
+  const [editedRole, setEditedRole] = useState('');
+
+  useEffect(() => {
+    const handleFetch = async () => {
+      try {
+        const response = await ApiService.getUsers();
+        setUsers(response.data);
+        setAllUsers(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Failed to fetch users:', error);
+      }
+    };
+
+    handleFetch();
+  }, []);
+
+  const handleSearch = async (search: string) => {
+    try {
+      const searchResult = allUsers.filter((user) =>
+        user.username.toLowerCase().includes(search.toLowerCase()));
+      setUsers(searchResult);
+    } catch (error) {
+      console.error('Failed to search users:', error);
+    }
+  };
+
+  const handleEdit = (id: number, role: string) => {
+    setEditingId(id);
+    setEditedRole(role);
+  };
+
+  const handleUpdate = async (id: number) => {
+    try {
+      await ApiService.updateUser(id, { role: editedRole } as User);
+      const updatedUsers = users.map((user) => {
+        if (+user.id === id) {
+          return { ...user, role: editedRole };
+        }
+        return user;
+      });
+      setUsers(updatedUsers);
+      setEditingId(null);
+      Swal.fire({
+        icon: 'success',
+        title: 'User updated successfully',
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    } catch (error) {
+      console.error('Failed to update user:', error);
+    }
+  };
+
+  const handleDelete = async (id: number) => {
+    const result = await handleSwalDelete();
+
+    if (!result.isConfirmed) return;
+
+    try {
+      await ApiService.deleteUser(id);
+      const deletedUser = users.filter((user) => +user.id !== id);
+      setUsers(deletedUser);
+      Swal.fire({
+        icon: 'success',
+        title: 'User deleted successfully',
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    } catch (error) {
+      console.error('Failed to delete user:', error);
+    }
+  };
+
+  const handleSwalDelete = () => {
+    return Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#000',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    });
+  };
+
+  const handleLogout = () => {
+    localStorage.clear();
+    setUsers([]);
+    navigate('/');
+  };
+
+  return {
+    users,
+    loading,
+    editingId,
+    editedRole,
+    handleSearch,
+    handleEdit,
+    handleUpdate,
+    handleDelete,
+    handleLogout,
+    setEditedRole,
+  };
+};
+
+export default useDashboard;
