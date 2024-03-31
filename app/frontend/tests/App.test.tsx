4a5d00a4100f059397f51e4e758d70a6c7dbc7a0
rafael <rafaelmagalhaes1@live.com>
2024-03-31 14:07:54 -0300

    tests: login page tested

--- app/frontend/tests/App.test.tsx
+++ app/frontend/tests/App.test.tsx
@@ -1,7 +1,60 @@
-import { render, screen } from '@testing-library/react';
 import App from '../src/App';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { renderWithRouter } from './renderWithRouter';
 
-it('should show hello world', () => {
-  render(<App />);
-  expect(screen.getByText('Hello, World!')).toBeInTheDocument();
-})
+// Informations from Login Page
+const TITLE = 'User Manager System';
+const SUBTITLE = 'Manage your users in a simple and easy way';
+const TEST_CREDENTIALS = 'Test the system with credentials';
+const CREDENTIALS = 'rick@admin.com / secret_admin';
+
+// Form elements
+const TITLE_FORM = 'User Login';
+const EMAIL_INPUT = 'Email';
+const PASSWORD_INPUT = 'Password';
+const BUTTON_LOGIN = 'Login';
+
+describe('Tests Login Page', () => {
+  it('1.1 should render elements information login page', () => {
+    renderWithRouter(<App />);
+    expect(screen.getByText(TITLE)).toBeInTheDocument();
+    expect(screen.getByText(SUBTITLE)).toBeInTheDocument();
+    expect(screen.getByText(TEST_CREDENTIALS)).toBeInTheDocument();
+    expect(screen.getByText(CREDENTIALS)).toBeInTheDocument();
+  })
+
+  it('1.2 should render form elements', () => {
+    renderWithRouter(<App />);
+    expect(screen.getByText(TITLE_FORM)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(EMAIL_INPUT)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(PASSWORD_INPUT)).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: BUTTON_LOGIN })).toBeInTheDocument();
+  });
+
+  it('1.3 should login with success', async () => {
+    renderWithRouter(<App />);
+    const email = 'rick@admin.com';
+    const password = 'secret_admin';
+    const buttonLogin = screen.getByRole('button', { name: BUTTON_LOGIN });
+    const emailInput = screen.getByPlaceholderText(EMAIL_INPUT);
+    const passwordInput = screen.getByPlaceholderText(PASSWORD_INPUT);
+
+    userEvent.type(emailInput, email);
+    userEvent.type(passwordInput, password);
+    userEvent.click(buttonLogin);
+  });
+
+  it('1.4 should login success and redirect to home page', async () => {
+    renderWithRouter(<App />);
+    const email = 'rick@admin.com';
+    const password = 'secret_admin';
+    const buttonLogin = screen.getByRole('button', { name: BUTTON_LOGIN });
+    const emailInput = screen.getByPlaceholderText(EMAIL_INPUT);
+    const passwordInput = screen.getByPlaceholderText(PASSWORD_INPUT);
+
+    userEvent.type(emailInput, email);
+    userEvent.type(passwordInput, password);
+    userEvent.click(buttonLogin);
+  });
+});
