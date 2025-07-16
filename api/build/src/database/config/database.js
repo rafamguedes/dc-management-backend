474201fcb0241aeb1cf10dae58fd4bb91912b896
Rafael Guedes <rafaelmagalhaes1@live.com>
2025-07-15 22:41:15 -0300

    fix: variables

--- api/build/src/database/config/database.js
+++ api/build/src/database/config/database.js
@@ -1,11 +1,11 @@
 "use strict";
 const config = {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'root',
-    database: process.env.DB_NAME || 'auth_db',
-    host: process.env.DB_HOST || 'localhost',
-    port: Number(process.env.DB_PORT) || 3306,
-    dialect: 'mysql',
+    username: process.env.PGUSER || 'root',
+    password: process.env.PGPASSWORD || 'root',
+    database: process.env.PGDATABASE || 'auth_db',
+    host: process.env.PGHOST || 'localhost',
+    port: Number(process.env.PGPORT) || 5432,
+    dialect: 'postgres',
 };
 module.exports = config;
 //# sourceMappingURL=database.js.map
