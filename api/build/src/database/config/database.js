695e80e59264a1f8bd99233cc2ae2f94a7bd67ed
Rafael Guedes <rafaelmagalhaes1@live.com>
2025-07-16 20:32:16 -0300

    fix: db config

--- api/build/src/database/config/database.js
+++ api/build/src/database/config/database.js
@@ -1,13 +1,22 @@
 "use strict";
 const dotenv = require("dotenv");
+const pg_connection_string_1 = require("pg-connection-string");
 dotenv.config();
+const databaseUrl = process.env.DATABASE_URL;
+const parsedUrl = databaseUrl ? (0, pg_connection_string_1.parse)(databaseUrl) : null;
 const config = {
-    username: process.env.PGUSER || 'postgres',
-    password: process.env.PGPASSWORD || 'postgres',
-    database: process.env.PGDATABASE || 'dc_management_db',
-    host: process.env.PGHOST || 'localhost',
-    port: Number(process.env.PGPORT) || 5432,
+    username: (parsedUrl === null || parsedUrl === void 0 ? void 0 : parsedUrl.user) || process.env.PGUSER || 'postgres',
+    password: (parsedUrl === null || parsedUrl === void 0 ? void 0 : parsedUrl.password) || process.env.PGPASSWORD || 'postgres',
+    database: (parsedUrl === null || parsedUrl === void 0 ? void 0 : parsedUrl.database) || process.env.PGDATABASE || 'dc_management_db',
+    host: (parsedUrl === null || parsedUrl === void 0 ? void 0 : parsedUrl.host) || process.env.PGHOST || 'localhost',
+    port: (parsedUrl === null || parsedUrl === void 0 ? void 0 : parsedUrl.port) ? Number(parsedUrl.port) : Number(process.env.PGPORT) || 5432,
     dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        }
+    },
 };
 module.exports = config;
 //# sourceMappingURL=database.js.map
--- api/src/database/config/database.ts
+++ api/src/database/config/database.ts
@@ -1,15 +1,25 @@
 import { Options } from 'sequelize';
 import * as dotenv from 'dotenv';
+import { parse } from 'pg-connection-string';
 
 dotenv.config();
 
+const databaseUrl = process.env.DATABASE_URL;
+const parsedUrl = databaseUrl ? parse(databaseUrl) : null;
+
 const config: Options = {
-  username: process.env.PGUSER || 'postgres',
-  password: process.env.PGPASSWORD || 'postgres',
-  database: process.env.PGDATABASE || 'dc_management_db',
-  host: process.env.PGHOST || 'localhost',
-  port: Number(process.env.PGPORT) || 5432,
+  username: parsedUrl?.user || process.env.PGUSER || 'postgres',
+  password: parsedUrl?.password || process.env.PGPASSWORD || 'postgres',
+  database: parsedUrl?.database || process.env.PGDATABASE || 'dc_management_db',
+  host: parsedUrl?.host || process.env.PGHOST || 'localhost',
+  port: parsedUrl?.port ? Number(parsedUrl.port) : Number(process.env.PGPORT) || 5432,
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    }
+  },
 };
 
 export = config;
