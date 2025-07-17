638d559b0981d4376ec509957e32cec5b36d284f
Rafael Guedes <rafaelmagalhaes1@live.com>
2025-07-16 21:50:07 -0300

    chore: test cors config

--- api/src/middlewares/CorsMiddleware.ts
+++ api/src/middlewares/CorsMiddleware.ts
@@ -2,25 +2,9 @@ import express = require('express');
 
 const createCorsMiddleware = (): express.RequestHandler => {
   return (req, res, next) => {
-    const allowedOrigins = ['*'];
-
-    const origin = req.headers.origin;
-    
-    if (allowedOrigins.includes(origin as string) || !origin) {
-      res.header('Access-Control-Allow-Origin', origin || '*');
-    }
-
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Max-Age', '86400');
-
-    if (req.method === 'OPTIONS') {
-      res.sendStatus(200);
-      return;
-    }
-
+    res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+    res.header('Access-Control-Allow-Headers', '*');
     next();
   };
 };
