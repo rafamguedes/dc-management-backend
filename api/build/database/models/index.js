5a3ce5116d535ec1c89d82e86c13c2cf77252728
Rafael Guedes <rafaelmagalhaes1@live.com>
2025-07-20 14:09:04 -0300

    rebuild

--- api/build/database/models/index.js
+++ api/build/database/models/index.js
@@ -3,22 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const config = require("../config/database");
 const sequelize = new sequelize_1.Sequelize(config);
-// Add connection event handlers
-sequelize.addHook('beforeConnect', (config) => {
-    console.log('Attempting to connect to database...');
-});
-sequelize.addHook('afterConnect', (connection, config) => {
-    console.log('Successfully connected to database');
-});
-sequelize.addHook('beforeDisconnect', (connection) => {
-    console.log('Disconnecting from database...');
-});
-sequelize.addHook('afterDisconnect', (connection) => {
-    console.log('Disconnected from database');
-});
-// Handle connection errors
-process.on('unhandledRejection', (reason, promise) => {
-    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
-});
 exports.default = sequelize;
 //# sourceMappingURL=index.js.map
