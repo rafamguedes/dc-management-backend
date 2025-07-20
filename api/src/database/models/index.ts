a812abb679d0dbe5c8bce94d45177f6d8dbccb59
Rafael Guedes <rafaelmagalhaes1@live.com>
2025-07-20 14:07:43 -0300

    refact: sequelize config

--- api/src/database/models/index.ts
+++ api/src/database/models/index.ts
@@ -3,26 +3,4 @@ import * as config from '../config/database';
 
 const sequelize = new Sequelize(config);
 
-// Add connection event handlers
-sequelize.addHook('beforeConnect', (config) => {
-  console.log('Attempting to connect to database...');
-});
-
-sequelize.addHook('afterConnect', (connection, config) => {
-  console.log('Successfully connected to database');
-});
-
-sequelize.addHook('beforeDisconnect', (connection) => {
-  console.log('Disconnecting from database...');
-});
-
-sequelize.addHook('afterDisconnect', (connection) => {
-  console.log('Disconnected from database');
-});
-
-// Handle connection errors
-process.on('unhandledRejection', (reason, promise) => {
-  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
-});
-
 export default sequelize;
