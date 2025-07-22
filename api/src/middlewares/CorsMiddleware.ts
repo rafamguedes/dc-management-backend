7a90550a9174c8168201b1133f67ad2a7096d943
Rafael Guedes <rafaelmagalhaes1@live.com>
2025-07-22 20:39:26 -0300

    chore: config cors headers

--- api/src/middlewares/CorsMiddleware.ts
+++ api/src/middlewares/CorsMiddleware.ts
@@ -4,7 +4,20 @@ const createCorsMiddleware = (): express.RequestHandler => {
   return (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
-    res.header('Access-Control-Allow-Headers', '*');
+    
+    res.header(
+      'Access-Control-Allow-Headers', 
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma'
+    );
+    
+    res.header('Access-Control-Allow-Credentials', 'true');
+    
+    res.header('Access-Control-Max-Age', '86400');
+    
+    if (req.method === 'OPTIONS') {
+      return res.status(200).end();
+    }
+    
     next();
   };
 };
